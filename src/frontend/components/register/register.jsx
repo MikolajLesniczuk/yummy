c1b4f99ba2a221f4eb385d44068adec2634acdaa
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-10-25 21:06:03 +0200

    work

--- src/frontend/components/register/register.jsx
+++ src/frontend/components/register/register.jsx
@@ -3,6 +3,7 @@ import s from './register.module.css';
 export const Register = () => {
   return (
     <div className={s.container}>
+      register tutaj
       <div>
         <div></div>
         <div></div>
